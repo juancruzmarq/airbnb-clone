587f42989bba292ce355d98f149aabd683db328f
Juan Cruz <marquezjuanchy@hotmail.com>
2022-10-14 14:43:42 -0300

    day one build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  // declarar los dominios a los que se puede acceder desde el navegador
+  images: {
+    domains: ["links.papareact.com", "a0.muscache.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+};
